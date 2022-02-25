6781cd21ff2a5a489c59506414b25a578a9463f7
Kaushalendra Singh <92047999+mAdrIs2@users.noreply.github.com>
2022-02-25 16:28:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function diceGame(){
+
   var randomNumber1 = Math.floor((Math.random() * 6)) + 1;
 
 document.querySelector("img.img1").setAttribute("src", 'images/' + 'dice' + randomNumber1 + '.png');
@@ -6,6 +7,8 @@ document.querySelector("img.img1").setAttribute("src", 'images/' + 'dice' + rand
 var randomNumber2 = Math.floor((Math.random() * 6)) + 1;
 
 document.querySelector("img.img2").setAttribute("src", 'images/' + 'dice' + randomNumber2 + '.png');
+
+document.querySelector("h1").classList.add("blink_me");
 
 if (randomNumber1 > randomNumber2)
 
@@ -16,5 +19,5 @@ else if (randomNumber1 < randomNumber2)
   document.querySelector("h1").innerHTML = "Player 2 Wins🔻";
 
 else
-  document.querySelector("h1").innerHTML = "Draw!";
+  document.querySelector("h1").innerHTML = "😂Draw!😂";
 }
